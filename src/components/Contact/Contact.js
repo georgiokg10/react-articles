5fe7ce236f8b3db3efa616d08c88e08743aef3cd
supermario26 <84723755+supermario26@users.noreply.github.com>
2021-05-31 18:02:12 +0300

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import hedgehog from "./../../assets/images/hedgehog.jpg";
 
-const Contact = () => {
-  return <div className="content">Contact Us</div>;
-};
+const Footer = () =>
+  <div className="footer">
+    <span className="footer-font mt-4 ml-3">Georgios Kontogeorgos (DG11),</span>
+    <img src={hedgehog} alt="" className="footer-ico"></img><br />
+    <span className="footer-font mt-4 ml-3">Marios Katsanos (DG8),</span>
+    <img src={hedgehog} alt="" className="footer-ico"></img><br /> 
+    <span className="footer-font mt-4 ml-3">Margarita Syntila (DG1) </span>
+    <img src={hedgehog} alt="" className="footer-ico"></img>
+  </div>
 
-export default Contact;
+export default Footer;
