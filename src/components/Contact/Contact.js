86ef8f03fdf9055693169e1122a1f1441318d809
George Kontogeorgos <georgioskont@hotmail.gr>
2021-05-31 23:28:17 +0300

    fixes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import hedgehog from "./../../assets/images/hedgehog.jpg";
 
-const Footer = () =>
+const Contact = () =>
   <div className="footer">
-    <span className="footer-font mt-4 ml-3">Georgios Kontogeorgos (DG11),</span>
+    {/* <span className="footer-font mt-4 ml-3">Georgios Kontogeorgos (DG11),</span>
     <img src={hedgehog} alt="" className="footer-ico"></img><br />
     <span className="footer-font mt-4 ml-3">Marios Katsanos (DG8),</span>
     <img src={hedgehog} alt="" className="footer-ico"></img><br /> 
     <span className="footer-font mt-4 ml-3">Margarita Syntila (DG1) </span>
-    <img src={hedgehog} alt="" className="footer-ico"></img>
+    <img src={hedgehog} alt="" className="footer-ico"></img> */}
   </div>
 
-export default Footer;
+export default Contact;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,8 @@ import hedgehog from "./../../assets/images/hedgehog.jpg";
 const Footer = () =>
   <div className="footer">
     <span className="footer-font mt-4 ml-3">Georgios Kontogeorgos (DG11), </span>
+    <span className="footer-font mt-4 ml-3">Marios Katsanos (DG8), </span>
+    <span className="footer-font mt-4 ml-3">Margarita Syntila (DG1) </span>
     <img src={hedgehog} alt="" className="footer-ico"></img>
   </div>
 
