54c7121e5744b799b92db5539dc8ea8897ab088a
KONTOGEORGOS Georgios <georgioskont@hotmail.gr>
2021-06-09 13:23:40 +0300

    reformat

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <span className="footer-font mt-4 ml-3">
-      Georgios Kontogeorgos (DG11),{" "}
-    </span>
+    <span className="footer-font mt-4 ml-3">Georgios Kontogeorgos (DG11), </span>
     <span className="footer-font mt-4 ml-3">Marios Katsanos (DG8), </span>
     <span className="footer-font mt-4 ml-3">Margarita Syntila (DG1) </span>
   </div>
